8a68c95777c9cb45aceedd8d0fc31bafcd33cf69
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-06 16:17:52 -0300

    Button Move
    Quando clica no slot o objeto acompanha o mouse e desativa o script do botao

--- InventarioTest/Assets/Scripts/Inventory.cs
+++ InventarioTest/Assets/Scripts/Inventory.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+
+    public GameObject mouseItem;
+
     public void DragItem(GameObject button)
     {
-
+        mouseItem = button;
+        mouseItem.transform.position = Input.mousePosition;
     }
     public void DropItem(GameObject button)
     {
