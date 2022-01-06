227604b277ea3846c13bd6c71d232e690107625a
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-06 16:05:31 -0300

    Duplicar Slot
    Dupliquei o slot e criei o script inventory

---
+++ InventarioTest/Assets/Scripts/Inventory.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public void DragItem(GameObject button)
+    {
+
+    }
+    public void DropItem(GameObject button)
+    {
+
+    }
+}
