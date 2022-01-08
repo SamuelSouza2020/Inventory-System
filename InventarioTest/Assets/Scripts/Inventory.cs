a47d934baec2079fd1ddfabd756d2978095e200f
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-08 12:30:23 -0300

    Acompanhar Quantia
    O texto acompanha o item mostrando a quantidade

--- InventarioTest/Assets/Scripts/Inventory.cs
+++ InventarioTest/Assets/Scripts/Inventory.cs
@@ -8,31 +8,37 @@ public class Inventory : MonoBehaviour
 
     public Item[] item;
     //test
-    public GameObject[] slot;
+    //public GameObject[] slot;
 
-    public GameObject[] txtSlot;
+    //public GameObject[] txtSlot;
 
-    public Text[] txtSQ;
+    //public Text[] txtSQ;
 
     public GameObject mouseItem;
     int cont = 0;
 
     private void Start()
     {
-        slot = new GameObject[6];
-        txtSlot = new GameObject[6];
+        //slot = new GameObject[6];
+        /*txtSlot = new GameObject[6];
         txtSQ = new Text[6];
         for(int x = 1; x < 7; x++)
         {
-            slot[cont = x - 1] = GameObject.Find("Slot" + x);
-            txtSlot[cont = x - 1] = slot[cont = x - 1].transform.GetChild(0).transform.GetChild(0).gameObject;
+            //slot[cont = x - 1] = GameObject.Find("Slot" + x);
+            txtSlot[cont = x - 1] = item[cont = x - 1].ga.transform.GetChild(0).transform.GetChild(0).gameObject;
             txtSQ[cont = x - 1] = txtSlot[cont].GetComponent<Text>();
-        }
+        }*/
     }
     private void Update()
     {
-        for(int x = 1; x < 7; x++)
-            txtSQ[cont = x - 1].text = item[cont = x - 1].quantidade.ToString();
+
+        /*for(int x = 1; x < 7; x++)
+            txtSQ[cont = x - 1].text = item[cont = x - 1].quantidade.ToString();*/
+
+        for(int x = 0; x < 6; x++)
+        {
+            item[x].txtQuant.text = item[x].quantidade.ToString();
+        }
     }
     public void DragItem(GameObject button)
     {
@@ -56,7 +62,7 @@ public class Inventory : MonoBehaviour
     }
     public void ConsumeItem()
     {
-        if(UIManager.instance.pyMoney > 0)
+        if (UIManager.instance.pyMoney > 0)
         {
             //Verificar o objeto consumido e subtrair
             for (int a = 0; a < 6; a++)
--- InventarioTest/Assets/Scripts/Item.cs
+++ InventarioTest/Assets/Scripts/Item.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Item
 {
     public string id;
     public int quantidade;
+    public Text txtQuant;
     public int iAttack;
     public int iLife;
     public int iRes;
